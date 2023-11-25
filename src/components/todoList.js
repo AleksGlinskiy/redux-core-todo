bec5638e518676901d80432d81464971fb2f290d
Алексей <a.glinskiy@st-lt.ru>
2023-11-26 02:03:23 +0300

    add styles

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -5,14 +5,14 @@ const todoList = (todo_list_container, todos) => {
 
     if (!todos.length) {
         const empty_message = todo_list_container.getAttribute('data-empty-message');
-        todo_list_container.innerText = empty_message ? empty_message : '';
+        todo_list_container.innerHTML = `<div class="empty-list">${empty_message ? empty_message : ''}</div>`;
         return;
     }
 
     return todos.map((todo) => {
         const todo_item = document.createElement("div");
         const todo_item_btn = document.createElement("button");
-        todo_item_btn.classList.add('todo-block__btn');
+        todo_item_btn.classList.add('todo-block__btn', 'btn-ico');
 
         todo_item.classList.add('todo-block');
         if (todo.completed) {
@@ -21,7 +21,7 @@ const todoList = (todo_list_container, todos) => {
 
         todo_item.setAttribute('data-id', todo.id);
         todo_item.textContent = todo.text;
-        todo_item_btn.textContent = 'X';
+        todo_item_btn.innerHTML = '&#10006;';
 
         todo_item.appendChild(todo_item_btn);
         todo_list_container.appendChild(todo_item);
